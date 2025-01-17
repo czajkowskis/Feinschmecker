ef57d277c57494a83482d7a4510a195cb985c7d2
czajkowskis <szymonczajkowski022@gmail.com>
2025-01-17 17:42:21 +0100

    Added vueuse/motion

--- webui/src/main.js
+++ webui/src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import axios from '../services/axios.js';
-
+import { MotionPlugin } from '@vueuse/motion'
 const app = createApp(App)
 
 app.config.globalProperties.$axios = axios;
+app.use(MotionPlugin);
 app.mount('#app')
