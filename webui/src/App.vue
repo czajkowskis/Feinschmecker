0244c8ad4c75b6aa70525ca5b439c0995848fc43
czajkowskis <szymonczajkowski022@gmail.com>
2025-01-17 11:47:28 +0100

    Added Search Section component

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -2,12 +2,14 @@
   import Navbar from "./components/Navbar.vue"
   import HeroHeader from "./components/HeroHeader.vue"
   import AboutUs from "./components/AboutUs.vue"
+  import SearchSection from "./components/SearchSection.vue"
 </script>
 
 <template>
   <Navbar/>
   <HeroHeader/>
   <AboutUs/>
+  <SearchSection/>
 </template>
 
 <style scoped>
---
+++ webui/src/components/SearchSection.vue
@@ -0,0 +1,66 @@
+<script setup>
+</script>
+
+<template>
+  <section class="search-section">
+    <div class="container">
+      <h1>Start your search</h1>
+      <div class="top-container">
+        <p>
+          Here you can enter your requirements and our system will 
+          return to you with tasty recipes tailored to your needs! If 
+          some aspect is not important for you leave the input blank. 
+        </p>
+      </div>
+      <div class="bottom-container">
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+  
+  .hero-section {
+    width: 100%;
+    margin:0;
+  }
+  
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 90%;
+    margin: 50px auto;
+  }
+
+  .top-container {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 20px 0;
+  }
+
+
+  .bottom-container {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 20px 0;
+  }
+
+  h1 {
+    font-family: "Poppins";
+    font-size: 48px;
+    font-weight: 700;
+    margin: 30px 0;
+  }
+
+  p {
+    font-family: "Poppins";
+    font-size: 28px;
+    font-weight: 400;
+    text-align: justify;
+  }
+
+</style>
