d2b5deb0826fa433320f521ad89da0a83fd28384
czajkowskis <szymonczajkowski022@gmail.com>
2025-01-17 16:08:12 +0100

    Added axios

--- webui/src/main.js
+++ webui/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import axios from '../services/axios.js';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.config.globalProperties.$axios = axios;
+app.mount('#app')
