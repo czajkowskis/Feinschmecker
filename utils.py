8866ca944eb49462bdb94aeb774fea912ae89439
imbhuvnesh <vbhuvenesh05@gmail.com>
2025-01-18 22:43:20 +0100

    fix time issue

--- utils.py
+++ utils.py
@@ -33,13 +33,22 @@ def separate_nutrition(nutritions):
 
 
 def get_time(cooking_time):
-    times = []
+    total_minutes = 0
+    
     for item in cooking_time:
-        time_text = item.text
-        number = int(time_text.split()[0])
-        times.append(number)
+        time_text = item.text.lower() 
+        parts = time_text.split()
 
-    return max(times)
+        minutes = 0
+        for i, part in enumerate(parts):
+            if "hr" in part:  
+                minutes += int(parts[i - 1]) * 60
+            elif "min" in part:  
+                minutes += int(parts[i - 1])
+
+        total_minutes += minutes
+
+    return total_minutes
 
 
 def parse_ingredients(ingredient_texts):
